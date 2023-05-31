7216a2a64f727847e6b699bc24fc33003486c815
dododoyb <128238091+dododoyb@users.noreply.github.com>
2023-05-31 20:33:16 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,51 +1,11 @@
-package b.global;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
-
-	public static void main(String[] args) {
-		TimerStorage.getInstance().newTimerTask("main proc");
-		TimerStorage.getInstance().newTimerTask("sub1");
-		TimerStorage.getInstance().newTimerTask("sub2");
-
-		TimerStorage.getInstance().start("main proc");
-		
-		// sub1の1回目
-		TimerStorage.getInstance().start("sub1");
-		sub(1);
-		TimerStorage.getInstance().end("sub1");
-
-		TimerStorage.getInstance().start("sub1");
-		// sub1の2回目
-		sub(2);
-		TimerStorage.getInstance().end("sub1");
-
-		TimerStorage.getInstance().start("sub2");
-		// sub2の1回目
-		sub(1);
-		TimerStorage.getInstance().end("sub2");
-
-		TimerStorage.getInstance().start("sub2");
-		// sub2の2回目
-		sub(2);
-		TimerStorage.getInstance().end("sub2");
-
-		TimerStorage.getInstance().start("sub2");
-		// sub2の3回目
-		sub(3);
-		TimerStorage.getInstance().end("sub2");
-
-//		TimerStorage.getInstance().end("main proc");
-		TimerStorage.getInstance().printTimerReport();
-
-		
-	}
-
-	static void sub(int seconds) {
-		try {
-			Thread.sleep(seconds * 1000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
-
+    public static void main(String[] args) {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd HH:mm:ss");
+        String formattedDateTime = now.format(formatter);
+        System.out.println(formattedDateTime);
+    }
 }
